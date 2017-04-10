f51aae2a9bd7fe244684ff043de2bc6d7785551d
EnrikeGonzaloLegarra <enrikeglegarra@gmail.com>
2017-04-10 11:06:06 +0200

    / route created

--- uciApi/routes/api-uci/men/menCT.js
+++ uciApi/routes/api-uci/men/menCT.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var Xray = require("x-ray");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  var xray = new Xray();
+  xray(`http://www.procyclingstats.com/teams`, `.wrapper`, {
+    title: ['.black'],
+    link: ['.black@href'],
+    img: ['img@src']
+  })(function(err, obj, team) {
+    var team = new Object();
+    for (var i = 18; i < 40; i++) {
+      team["title"] = obj.title[i]
+      team["link"] = obj.link[i]
+      team["img"] = obj.img[i + 1]
+      console.log("-----------------------------------");
+      console.log(team.title);
+      console.log(team.link);
+      console.log(team.img);
+      console.log("-----------------------------------");
+    }
+
+  })
   res.send('UCI Men ConTinental  Teams');
 });
 
--- uciApi/routes/api-uci/men/menWT.js
+++ uciApi/routes/api-uci/men/menWT.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Xray = require("x-ray");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('UCI Men WorldTour Teams');
+  var xray = new Xray();
+  xray(`http://www.procyclingstats.com/teams`, `.wrapper`, {
+    title: ['.black'],
+    link: ['.black@href'],
+    img: ['img@src']
+  })(function(err, obj, team) {
+    var team = new Object();
+    for (var i = 0; i < 18; i++) {
+      team["title"] = obj.title[i]
+      team["link"] = obj.link[i]
+      team["img"] = obj.img[i + 1]
+      console.log("-----------------------------------");
+      console.log(team.title);
+      console.log(team.link);
+      console.log(team.img);
+      console.log("-----------------------------------");
+    }
+
+  })
+  res.send('UCI MEN WorldTour Team');
 });
 
 module.exports = router;
